625c15943424c4f9740475752a7a9efe3f10e561
sabi wasthi <sabiwasthi@gmail.com>
2026-06-05 18:00:19 +0545

    Add WhatsApp call link to thank-you page

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const vimeoLink = "";
-const whatsappLink = "";
+const whatsappLink = "https://call.whatsapp.com/voice/mVRm6GxGrsof1xqMvyFIbu";
 
 export default function ThanksPage() {
   return (
